f39c7819d8c5c77e8b3c0c6d2e99d94725926b85
CMogore <chelsea.mogore@gmail.com>
2024-05-14 16:19:14 +0300

    connecting to database

---
+++ backend/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app =  express();
+const { sequelize } = require('./models/'); // Import Sequelize instance and models
+const PORT = process.env.PORT || 3000;
+
+var corOptions = {
+    origin: 'http://localhost:8080/'
+}
+
+app.use(cors(corOptions));
+// Middleware
+app.use(bodyParser.json());
+
+//testing api
+app.get('/' , (req, res) => {
+    res.json({message: 'helloooo'})
+});
+
+
+
+sequelize
+  .sync() // Sync Sequelize models with database
+  .then(() => {
+    console.log('Database synced');
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Database sync error:', error);
+  });
---
+++ backend/config/database.js
@@ -0,0 +1,7 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('moviesApp', 'root', 'password', {
+    host: 'localhost',
+    dialect: 'mysql' // Use the appropriate database dialect
+  });
+module.exports = sequelize;
